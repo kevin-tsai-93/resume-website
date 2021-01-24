3b1ff5231ad59ce9aeb532a9759fee98612c47ca
kevin-tsai-93 <77864463+kevin-tsai-93@users.noreply.github.com>
2021-01-23 19:07:36 -0800

    Add files via upload

--- components/index/Resume.js
+++ components/index/Resume.js
@@ -8,7 +8,7 @@ const Resume = () => {
             <section className="about-me">
                 <h4>About me</h4>
                 <p>Hi! I'm Kevin Tsai. Your friendly front-end web developer and software engineer. I enjoy building content on the web and solving problems through code. 
-                    I also have a math-heavy and data analytical background, having obtained both an M.S. and B.S. in Statistics. 
+                    I also have a math-heavy and data-analytical background, having obtained both an M.S. and B.S. in Statistics. 
                     Feel free to check out my portfolio. Hope to hear from you soon!</p>
             </section>
             <section className="contact-info">
@@ -67,8 +67,8 @@ const Resume = () => {
             <section className="toolkit">
                 <h4>Toolkit</h4>
                 <ul>
-                    <li className="tool">HTML, CSS, JavaScript, React</li>
-                    <li className="tool">Node, Express, jQuery, Rest, API, MongoDB</li>
+                    <li className="tool">HTML, CSS, JavaScript</li>
+                    <li className="tool">React, Node, Express, jQuery, Rest, API, MongoDB</li>
                     <li className="tool">Bootstrap, Semantic UI, Material-UI</li>
                     <li className="tool">C, C++, Java, Python, R</li>
                     <li className="tool">and more!</li>
@@ -86,11 +86,11 @@ const Resume = () => {
                 <ul>
                     <li className="certificate"><a href="https://coursera.org/share/3b01d11f71c8ad2bcff4981bb4d8b33b" target="_blank">DeepLearning.AI's Deep Learning Specialization @ Coursera</a></li>
                     <li className="certificate"><a href="https://coursera.org/share/b9ae4223e66a662e9ad2b3b62c76c5f5" target="_blank">Stanford's Machine Learning Course @ Coursera</a></li>
+                    <li className="certificate"><a href="https://coursera.org/share/a9e0f13f32a97951ef17c535018e341c" target="_blank">Imperial College London's Mathematics for Machine Learning: Linear Algebra Course @ Coursera</a></li>
                     <li className="certificate"><a href="https://coursera.org/share/fac2254a6ba508f9049fc6d9b81a1b7d" target="_blank">UC San Diego's Algorithmic Toolbox Course @ Coursera</a></li>
                     <li className="certificate"><a href="https://coursera.org/share/a3c5e3a4d33081a13c481dedd9432324" target="_blank">UC San Diego's Data Structures Course @ Coursera</a></li>
                     <li className="certificate"><a href="https://coursera.org/share/e5af870438b34d16205dd2fc4a31fa0a" target="_blank">UC San Diego's Algorithms on Graphs Course @ Coursera</a></li>
                     <li className="certificate"><a href="https://coursera.org/share/ab0770f27807f7b8888a86b91c69bb0a" target="_blank">UC San Diego's Algorithms on Strings Course @ Coursera</a></li>
-                    <li className="certificate"><a href="https://coursera.org/share/a9e0f13f32a97951ef17c535018e341c" target="_blank">Imperial College London's Mathematics for Machine Learning: Linear Algebra Course @ Coursera</a></li>
                 </ul>
             </section>
         </div>
