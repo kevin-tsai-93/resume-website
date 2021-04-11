c1aee9bda216473f5d2eb8291fb0d97c9632f39e
kevin-tsai-93 <77864463+kevin-tsai-93@users.noreply.github.com>
2021-04-10 17:23:21 -0700

    add components directory

---
+++ components/navbar/NavigationBar.js
@@ -0,0 +1,23 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const NavigationBar = () => {
+  return (
+    <Navbar bg="light" expand="lg" sticky="top" style={{borderRadius: 10}}>
+      <Navbar.Brand href="/">Resume</Navbar.Brand>
+      <Navbar.Toggle style={{outline: "none"}} aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Nav.Link href="/connect4">Connect 4</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/snake">Snake Game in Excel</Nav.Link>            
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
